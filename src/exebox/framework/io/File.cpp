eeaef0bf6c0484632968cbe4f35b270365c6da1e
Ron <get.snoop.scoped@gmail.com>
2024-10-12 23:42:30 +0300

    Framework: Add file utilties

---
+++ src/exebox/framework/io/File.cpp
@@ -0,0 +1,20 @@
+#include <fstream>
+
+#include "framework/io/File.h"
+
+namespace framework::io {
+
+std::vector<uint8_t> readFile(const std::filesystem::path& path)
+{
+	std::ifstream stream(path, std::ios::in | std::ios::binary);
+	const auto size = std::filesystem::file_size(path);
+
+	std::vector<uint8_t> content;
+	content.resize(size);
+
+	stream.read(reinterpret_cast<char*>(content.data()), size);
+
+	return content;
+}
+
+}
