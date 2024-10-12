0d5019e8cde4f1258e7a02247da127d46b21aaf4
Ron <get.snoop.scoped@gmail.com>
2024-10-12 23:53:50 +0300

    ExeBox: Add forward hooking functions

---
+++ src/exebox/exebox/hook/FixedAddress.h
@@ -0,0 +1,46 @@
+#pragma once
+
+#include <cstdint>
+
+namespace exebox::hook {
+
+struct FixedAddress
+{
+	/**
+		Creates a new fixed absolute address.
+
+		@param address - The absolute address.
+	 */
+	consteval FixedAddress(uintptr_t address);
+
+	/**
+		Converts the fixed address into a opaque pointer.
+	 */
+	operator void *() const;
+
+	/**
+		Converts the address to a typed address.
+	 */
+	template<typename T>
+	T *as();
+
+	uintptr_t address;
+};
+
+consteval FixedAddress::FixedAddress(const uintptr_t address)
+	: address(address)
+{}
+
+inline FixedAddress::operator void *() const
+{
+	return reinterpret_cast<void *>(address);
+}
+
+template<typename T>
+T *FixedAddress::as()
+{
+	return reinterpret_cast<T *>(address);
+}
+
+
+}
---
+++ src/exebox/exebox/hook/ForwardHook.cpp
@@ -0,0 +1,59 @@
+#include <algorithm>
+#include <ranges>
+
+#include "framework/log/Log.h"
+#include "framework/memory/ScopedMemoryProtection.h"
+
+#include "exebox/hook/ForwardHook.h"
+
+namespace exebox::hook {
+
+namespace {
+
+const std::vector<uint8_t> JUMP_INSTRUCTIONS = {
+	0xCC,						  // INT 3
+	0xB8, 0xFF, 0xFF, 0xFF, 0xFF, // MOVE EAX, 0xFFFFFFFF
+	0xFF, 0x20,					  // JMP DWORD PTR [EAX]
+};
+
+std::vector<uint8_t> createAbsoluteJumpIntrinsic(void *targetAddress)
+{
+	std::vector<uint8_t> instructions = JUMP_INSTRUCTIONS;
+	std::span view(reinterpret_cast<uint8_t *>(&targetAddress), sizeof(void *));
+
+	std::ranges::copy(view, instructions.data() + 2);
+
+	return instructions;
+}
+
+}
+
+void detail::ForwardHookRemover::operator()(void *ptr) const
+{
+	if(nullptr == ptr) {
+		return;
+	}
+
+	std::memcpy(ptr, previousContent.data(), previousContent.size());
+}
+
+ForwardHook::ForwardHook(FixedAddress hookAddress, void *functionAddress)
+	: m_HookFunction(functionAddress)
+{
+	std::vector<std::byte> previousContent;
+	{
+		auto memoryGuard = scopedWritableMemory(framework::VirtualMemoryView{ hookAddress, 256 });
+
+		auto hookInstructions = createAbsoluteJumpIntrinsic(&m_HookFunction);
+		previousContent.reserve(hookInstructions.size());
+		std::ranges::copy_n(hookAddress.as<std::byte>(), hookInstructions.size(), previousContent.data());
+
+		std::ranges::copy(hookInstructions, hookAddress.as<uint8_t>());
+	}
+
+	framework::log::info() << "Placed forward hook at: 0x" << std::hex << hookAddress.address;
+	m_Hook = { hookAddress, detail::ForwardHookRemover{ std::move(previousContent) } };
+}
+
+
+}
